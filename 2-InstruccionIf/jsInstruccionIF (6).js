5df38d0102ddee9a5f2be9f03025e64a95042cd8
christ1985 <36246925+christ1985@users.noreply.github.com>
2018-02-09 22:17:19 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,26 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+
+edad = document.getElementById("edad").value;
+
+if (( edad >= 13 ) && (edad <=17))
+{
+    alert("usted es adolescente");    
+}
+else
+{
+    if ( edad > 17)
+    {
+        alert("Usted es mayor de edad")
+    }
+    else 
+    {
+        alert("usted es un niño")
+    }
+    
+}
 
 
 
