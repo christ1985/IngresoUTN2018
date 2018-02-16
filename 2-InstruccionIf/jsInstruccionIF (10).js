379bc8f4a831b383b7467dff9c04bbd089c9fd93
christ1985 <36246925+christ1985@users.noreply.github.com>
2018-02-16 11:54:48 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,22 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numeroAleatorio;
 
+	numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+	
+	if (numeroAleatorio > 7)
+	{
+		alert("Excelente")
+	}		
+	else if (numeroAleatorio > 3)
+		{	
+		alert("aprobado")
+		}
+	else
+		{
+		alert("Vamos la proxima vez")
+		}
+	
+	
 }//FIN DE LA FUNCIÓN
