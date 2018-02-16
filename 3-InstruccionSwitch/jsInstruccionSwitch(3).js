95b0d491a3543cd3d7043fd89e861ee35adc0800
christ1985 <36246925+christ1985@users.noreply.github.com>
2018-02-16 19:34:04 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -5,12 +5,12 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 switch (mesDelAño){
-    case("Febrero"):
-    alert("Este mes no tiene mas de 29 dias");
-    break;
+    case "Febrero":
+        alert("Este mes no tiene mas de 29 dias");
+        break;
     default:
-    alert("Este mes tiene 30 o mas dias");
-    break;
+        alert("Este mes tiene 30 o mas dias");
+        break;
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,21 +5,21 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 switch(mesDelAño){
-    case("Enero"):
-    case("Marzo"):
-    case("Mayo"):
-    case("Julio"):
-    case("Agosto"):
-    case("Octubre"):
-    case("Diciembre"):
-    alert("Este mes tiene 31 dias");
-    break;
-    case("Febrero"):
-    alert("Este mes tiene 28 dias");
-    break;
+    case "Enero":
+    case "Marzo" :
+    case "Mayo" :
+    case "Julio" :
+    case "Agosto" :
+    case "Octubre" :
+    case "Diciembre" :
+        alert("Este mes tiene 31 dias");
+        break;
+    case "Febrero" :
+        alert("Este mes tiene 28 dias");
+        break;
     default:
-    alert("Este mes tiene 30 dias");
-    break;
+        alert("Este mes tiene 30 dias");
+        break;
 
 }	
 	
