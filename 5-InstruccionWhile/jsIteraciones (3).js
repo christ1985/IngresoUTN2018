9028c46df112cae677ca852c39ad5f9e03724bb7
christ1985 <36246925+christ1985@users.noreply.github.com>
2018-02-16 21:29:32 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -2,6 +2,13 @@ function Mostrar()
 {
 
 var clave = prompt("ingrese el número clave.");
-
-
+//while(!(clave == "utn750"))
+while(clave != "utn750")
+{
+    alert("Clave erronea, intentelo de nuevo");
+    clave=prompt("ingrese el número clave.");
+    // Podria ser tambien sin el alert y poner en el prompt el error
+    
+}
+alert("Clave correcta")
 }//FIN DE LA FUNCIÓN
