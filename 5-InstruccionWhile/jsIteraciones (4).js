ecbd020f2835d31c399fc95d01b120aa5e4f4fec
christ1985 <36246925+christ1985@users.noreply.github.com>
2018-02-16 22:11:10 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -3,5 +3,10 @@ function Mostrar()
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
 
-
+	while(!(numero <=10 && numero >0))
+	{
+		numero = prompt("Reingrese un valor entre 0 y 10.");
+	}
+	alert("Usted escogio el numero "+numero);
+	document. getElementById("Numero").value = numero;
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -7,4 +7,11 @@ var sexo = prompt("ingrese f ó m .");
 
 document.getElementById('Sexo').value=sexo;
 
+while(!(sexo == "f" && sexo == "m" ))
+{
+    sexo = prompt("reingrese f para femenino ó m para masculino .");
+}
+document.getElementById.value = sexo;
+
+
 }//FIN DE LA FUNCIÓN
